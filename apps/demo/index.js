b282110091d9294519d4970e4d198d35955d04db
Elliott Sprehn <esprehn@chromium.org>
2017-05-02 18:10:12 -0700

    Refactor to mediator.js

--- apps/demo/index.js
+++ apps/demo/index.js
@@ -1,7 +1,10 @@
 // Demo app.
 (async (global) => {
 
-let iframe = await apploader.createApp('helloworld');
-document.body.appendChild(iframe);
+let iframe1 = await apploader.createApp('helloworld');
+document.body.appendChild(iframe1);
+
+let iframe2 = await apploader.createApp('helloworld');
+document.body.appendChild(iframe2);
 
 })(self);
